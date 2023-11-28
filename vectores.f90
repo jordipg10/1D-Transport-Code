b592470bb8bf6c2c9ff161b23a5b8b8aa183052f
Jordi Petchamé <109295567+jordipg10@users.noreply.github.com>
2023-11-28 17:53:49 +0100

    Add files via upload

---
+++ vectores.f90
@@ -0,0 +1,63 @@
+module vectores_m
+    implicit none
+    save
+    contains
+        function outer_prod_vec(x,y) ! Computes outer product
+            implicit none
+            real(kind=8), intent(in) :: x(:), y(:) ! coefficients
+            real(kind=8), allocatable :: outer_prod_vec(:,:)
+            integer(kind=4) :: n,m,i,j
+            n=size(x)
+            m=size(y)
+            allocate(outer_prod_vec(n,m))
+            do i=1,n
+                do j=1,m
+                    outer_prod_vec(i,j)=x(i)*y(j)
+                end do
+            end do
+        end function outer_prod_vec
+        
+        function p_norm_vec(x,p) ! Computes p-norm
+            implicit none
+            real(kind=8), intent(in) :: x(:) ! vector
+            integer(kind=4), intent(in) :: p
+            real(kind=8) :: p_norm_vec
+            
+            integer(kind=4) :: i,n
+            real(kind=8) :: sum
+            real(kind=8), parameter :: epsilon=1d-9
+            if (p<1) error stop "p must be >= 1"
+            n=size(x)
+            sum=0d0
+            do i=1,n
+                sum=sum+abs(x(i))**p
+            end do
+            if (p==2) then
+                p_norm_vec=sqrt(sum)
+            else
+                p_norm_vec=sum**(1d0/p)
+            end if
+            !if (sum<epsilon) then
+            !    p_norm_vec=0d0
+            !else
+            !    p_norm_vec=sum**(1d0/p)
+            !end if
+        end function p_norm_vec
+        
+        function inf_norm_vec(x) ! Computes infinite norm
+            implicit none
+            real(kind=8), intent(in) :: x(:)
+            real(kind=8) :: inf_norm_vec
+            inf_norm_vec=maxval(abs(x))
+        end function 
+        
+        function proy_ortog(u,v) ! Computes orthogonal projection
+            real(kind=8), intent(in) :: u(:), v(:)
+            real(kind=8), allocatable :: proy_ortog(:)
+            integer(kind=4) :: n
+            if (size(u)/=size(v)) error stop "u and v must have same dimension"
+            n=size(u)
+            allocate(proy_ortog(n))
+            proy_ortog=(dot_product(u,v)/dot_product(v,v))*v
+        end function
+end module
