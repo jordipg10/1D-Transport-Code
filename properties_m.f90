cf1ebc91dba17f44e2661a3dde99cdb95f5a7c5a
Jordi Petchamé <109295567+jordipg10@users.noreply.github.com>
2023-11-29 21:09:38 +0100

    Update properties_m.f90

--- properties_m.f90
+++ properties_m.f90
@@ -13,27 +13,10 @@ module properties_m
         procedure, public :: set_source_term
         procedure, public :: set_source_term_order
         procedure, public :: set_source_term_flag
-        !procedure, public :: get_source_term
-        !procedure(compute_retardo), public, deferred :: compute_retardo
         procedure(read_props), public, deferred :: read_props
-        !procedure(get_dispersion), public, deferred :: get_dispersion
-        !procedure(compute_flux_1D), public, deferred :: compute_flux
-        !procedure, public :: set_caudal
     end type
     
     abstract interface
-        !subroutine set_trpt_props(this,source_term,retardo,porosity,flux,dispersion,spatial_discr_obj,time_discr_obj)
-        !    import props_c
-        !    import spatial_discr_c
-        !    import time_discr_c
-        !    implicit none
-        !    class(props_c) :: this
-        !    real(kind=8), intent(in) :: source_term,retardo
-        !    real(kind=8), intent(in), optional :: porosity(:),flux(:),dispersion(:)
-        !    class(spatial_discr_c), intent(in), optional :: spatial_discr_obj
-        !    class(time_discr_c), intent(in), optional :: time_discr_obj
-        !end subroutine
-    
     
         subroutine read_props(this,filename,spatial_discr)
             import props_c
@@ -42,22 +25,6 @@ module properties_m
             character(len=*), intent(in) :: filename
             class(spatial_discr_c), intent(in), optional :: spatial_discr
         end subroutine
-        
-      
-        
-        !subroutine compute_retardo(this,filename)
-        !    import props_c
-        !    implicit none
-        !    class(props_c) :: this
-        !    character(len=*), intent(in) :: filename
-        !end subroutine
-        
-        !function get_dispersion(this) result(dispersion)
-        !    import props_c
-        !    implicit none
-        !    class(props_c) :: this
-        !    real(kind=8), allocatable :: dispersion(:)
-        !end function
             
     end interface
     
@@ -94,17 +61,5 @@ module properties_m
         
         
         
-        !function get_source_term(this) result(r)
-        !    implicit none
-        !    class(props_c), intent(in) :: this
-        !    real(kind=8), allocatable :: r(:)
-        !    r=this%source_term
-        !end function
-        
-        !subroutine set_caudal(this,Q)
-        ! implicit none
-        !    class(props_c), intent(in) :: this
-        !    real(kind=8), intent(in) :: Q ! caudal
-        !    select type
         
 end module
