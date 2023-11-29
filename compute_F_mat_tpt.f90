95fbaa5f31ee33848562b1895f5ff0386c6390cd
Jordi Petchamé <109295567+jordipg10@users.noreply.github.com>
2023-11-29 21:10:21 +0100

    Update compute_F_mat_tpt.f90

--- compute_F_mat_tpt.f90
+++ compute_F_mat_tpt.f90
@@ -10,20 +10,12 @@ subroutine compute_F_mat_tpt(this)
     
     n=this%spatial_discr%Num_targets
      
-    !select type (props=>this%props)
-    !type is (tpt_props_homog_c)
     if (this%spatial_discr%adapt_ref==1) then
         deallocate(this%F_mat%diag)
         call this%F_mat%allocate_matrix(n)
     end if
     
     
-    this%F_mat%diag=this%tpt_props_heterog%porosity!*this%tpt_props_heterog%retardo
-    !type is (tpt_props_heterog_c)
-        !this%F_mat%diag=props%porosity*props%retardo
-    !type is (props_viena_c)
-    !    call this%F_mat%allocate_matrix(1)
-    !    this%F_mat%diag=props%porosity
-    !end select
+    this%F_mat%diag=this%tpt_props_heterog%porosity
         
 end subroutine
