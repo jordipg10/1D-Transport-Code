90e2e53204e2cb3696a7c7f4f8060f75861d2b10
Jordi Petchamé <109295567+jordipg10@users.noreply.github.com>
2023-11-29 21:11:25 +0100

    Update compute_B_mat.f90

--- compute_B_mat.f90
+++ compute_B_mat.f90
@@ -13,29 +13,12 @@ subroutine compute_B_mat(this,theta,B_mat,k)
     real(kind=8) :: B_norm_inf,B_norm_1
     
     n=this%spatial_discr%Num_targets
-    !print *, this%props%get_props()
     
-    !E_mat%sub=this%trans_mat%sub
-    !E_mat%diag=this%trans_mat%diag
-    !E_mat%super=this%trans_mat%super
-    !select type (time_discr=>this%time_discr)
-    !type is (time_discr_homog_c)
-    !    E_mat%sub=E_mat%sub*time_discr%Delta_t
-    !    E_mat%diag=E_mat%diag*time_discr%Delta_t
-    !    E_mat%super=E_mat%super*time_discr%Delta_t
-    !type is (time_discr_heterog_c)
-    !    E_mat%sub=E_mat%sub*time_discr%Delta_t(k)
-    !    E_mat%diag=E_mat%diag*time_discr%Delta_t(k)
-    !    E_mat%super=E_mat%super*time_discr%Delta_t(k)
-    !end select
-    !E_mat%diag=E_mat%diag/this%F_mat%diag
     call E_mat%allocate_matrix(n)
     call this%compute_E_mat(E_mat,k)
     B_mat%sub=(1d0-theta)*E_mat%sub
     B_mat%diag=1d0+(1d0-theta)*E_mat%diag
     B_mat%super=(1d0-theta)*E_mat%super
     
-    !B_norm_inf=B_mat%compute_norm_inf()
-    !B_norm_1=B_mat%compute_norm_1()
-    !print *, this%props%get_props()
+
 end subroutine 
